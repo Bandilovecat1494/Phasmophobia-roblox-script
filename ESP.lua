74a64e50584c08a9013dcd855fb551004d339a8a
Xero <110208892+Bandilovecat1494@users.noreply.github.com>
2023-03-20 15:01:10 -0700

    Update ESP.lua

--- ESP.lua
+++ ESP.lua
@@ -15,10 +15,9 @@ function sandbox(var,func)
 	return func
 end
 cors = {}
-mas = Instance.new("Model",game:GetService("Lighting"))
 Part0 = Instance.new("Part")
-Part0.Name = "GhostESP"
-Part0.Parent = mas
+Part0.Name = "ESPpart"
+Part0.Parent = workspace
 Part0.CFrame = CFrame.new(33.9500008, 3, -3.0539999, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 Part0.Position = Vector3.new(33.95000076293945, 3, -3.053999900817871)
 Part0.Size = Vector3.new(1.2000000476837158, 1.2000000476837158, 1.2000000476837158)
@@ -26,31 +25,24 @@ Part0.Anchored = true
 Part0.BottomSurface = Enum.SurfaceType.Smooth
 Part0.CanCollide = false
 Part0.TopSurface = Enum.SurfaceType.Smooth
-mas:Destroy()
+local ESP = Instance.new("Highlight")
+ESP.OutlineTransparency = 1
+ESP.FillTransparency = 0
+ESP.FillColor = Color3.fromRGB(136, 45, 45)
 for i,v in pairs(cors) do
 	spawn(function()
 		pcall(v)
 	end)
 end
 
-game["Run Service"].Heartbeat:Connect(function()
-	local Ghost
-	if game.Workspace:FindFirstChild("Ghost1") then
-		Ghost = game.Workspace:FindFirstChild("Ghost1")
-	else
-		if game.Workspace:FindFirstChild("Ghost2") then
-			Ghost = game.Workspace:FindFirstChild("Ghost2")
-		else
-			if game.Workspace:FindFirstChild("Ghost3") then
-				Ghost = game.Workspace:FindFirstChild("Ghost3")
-			else
-				if game.Workspace:FindFirstChild("Ghost4") then
-					Ghost = game.Workspace:FindFirstChild("Ghost4")
-				else
-					error("cannot find ghost! please use dex and look for Ghost,Number so like Ghost2 or Ghost1 and Rename it to Ghost1 And Press the Button again.")
-				end
-			end
-		end
+local Ghost
+for i,v in pairs(game.Workspace:GetDescendants()) do
+	if string.match(v.Name, "Ghost") or string.match(v.Name, "ghost") then
+		v.Name = "haha"
 	end
+end
+wait(0.1)
+Ghost = game.Workspace.haha
+game["Run Service"].Heartbeat:Connect(function()
 	Part0.Position = Ghost.HumanoidRootPart.Position
 end)
